163cbab905b7097bd18ffd27709e02e0cd4cc13e
Donna <donnajun1214@gmail.com>
2024-04-16 17:18:37 +0900

    [add] morgar for log

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const routes = require("./routers");
 const cookieParser = require("cookie-parser");
 const app = express();
+const morgan = require("morgan");
 
 // app
 //   .use(express.json())
@@ -26,6 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan("tiny"));
 app.get("/", (_, res) => {
   // for health check
   res.sendStatus(200);
